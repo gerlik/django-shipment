30284eeb7e8b90a8686d23ab15fbcfd7882750fc
Gerli <gerliks@gmail.com>
2024-06-13 17:11:30 +0300

    Add requirements.txt

--- shipment/settings.py
+++ shipment/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'shipment_db',
         'USER': 'postgres',
-        'PASSWORD': '',
+        'PASSWORD': 'test',  # too lazy to create .env today
         'HOST': 'localhost',
         'PORT': '5432',
     }
