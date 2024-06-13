f2afd5071b0095d0040f657daaae97bbaf092194
Gerli <gerliks@gmail.com>
2024-06-13 14:03:06 +0300

    Fix import

--- shipment-frontend/src/shipments/Shipments.js
+++ shipment-frontend/src/shipments/Shipments.js
@@ -3,7 +3,7 @@ import Badge from "react-bootstrap/Badge";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
-import Table from "react-bootstrap/Container";
+import Table from "react-bootstrap/Table";
 import {createShipment, deleteShipment, getShipments} from "../Service";
 import {Link} from "react-router-dom";
 
